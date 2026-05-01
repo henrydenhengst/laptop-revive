a5c8281262772ffb46df5cf8e20254762088dba7
Henry den Hengst <henrydenhengst@gmail.com>
2026-05-01 19:37:27 +0200

    Delete rmwifi.sh

--- files/rmwifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -uo pipefail
-
-echo -e "\nEventuele WiFi verbinding(en) verwijderen ... "
-for CONNECTION in $(nmcli -t -f TYPE,UUID connection show|awk 'BEGIN{FS=":"} $1~/wireless$/ {print $2}')
-do
-    nmcli connection delete "$CONNECTION"
-done
